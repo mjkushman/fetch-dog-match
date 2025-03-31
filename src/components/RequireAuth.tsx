4755255e20acec6e6bfdf4d9bea4344e17ccf167
mjkushman <mjkushman@gmail.com>
2025-03-31 11:56:26 -0700

    moved auth logic into its own component to separate concerns

---
+++ src/components/RequireAuth.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, useEffect, useState } from "react";
+import Login from "@/components/Login";
+import authenticate from "@/api/authenticate";
+import { LoginFormData } from "@/types/LoginFormData";
+
+export default function RequireAuth({children}:{children:ReactNode}) {
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+
+  const doLogin = async (loginFormData: LoginFormData) => {
+    setIsLoading(true);
+    try {
+      const response = await authenticate(loginFormData);
+      setIsAuthenticated(response);
+    } catch (error) {
+      console.error("Login failed", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+
+  useEffect(() => {
+    const url = new URL("/dogs/breeds/", BASE_URL);
+    fetch(url, { credentials: "include" })
+      .then((res) => {
+        if (res.status === 401) throw new Error(res.statusText);
+        return res.ok;
+      })
+      .then((res) => setIsAuthenticated(res))
+      .catch(() => setIsAuthenticated(false))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!isAuthenticated && !isLoading) return <Login doLogin={doLogin} />;
+  return <div>{children}</div>;
+}
