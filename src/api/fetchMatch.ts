d78f7475ae9a44fed199b22a4296f001a93cd790
mjkushman <mjkushman@gmail.com>
2025-03-31 23:35:21 -0700

    Added api call to match a dog

---
+++ src/api/fetchMatch.ts
@@ -0,0 +1,25 @@
+/** POST , returns a single ID from list of provided ids */
+
+import { Match } from "@/types/Match";
+/** Takes an array of dog IDs, returns an array of Dog objects */
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const fetchMatch = async (dogIds: string[]): Promise<Match> => {
+  const url = new URL("dogs/match", BASE_URL);
+
+  const result = await fetch(url, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dogIds),
+  });
+
+  if (!result.ok) {
+    throw new Error("Failed to fetch match");
+  }
+  return await result.json();
+};
+
+export default fetchMatch;
