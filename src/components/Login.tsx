c4a477e6c9a656ae03b9410be0c39291f9151d53
mjkushman <mjkushman@gmail.com>
2025-04-01 21:53:50 -0700

    updated Login screen. New components to support. shadcn

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import { useState } from "react";
 
 import { LoginFormData } from "@/types/LoginFormData";
-import { Field, Fieldset, Form, Input } from "@base-ui-components/react";
 
 
 type LoginProps = {
@@ -24,32 +34,58 @@ export default function Login({ doLogin }: LoginProps) {
   };
 
   return (
-    <div>
-      <Form onSubmit={handleSubmit} name="loginForm">
-        <Fieldset.Root>
-          <Fieldset.Legend>Log in to continue</Fieldset.Legend>
-          <Field.Root>
-            <Input
-              name="name"
-              placeholder="Name"
-              autoComplete="firstname"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </Field.Root>
-          <Field.Root>
-            <Input
-              name="email"
-              placeholder="Email"
-              autoComplete="email"
-              type="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </Field.Root>
-          <button type="submit">Login</button>
-        </Fieldset.Root>
-      </Form>
-    </div>
+    <>
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">
+          <div className="flex flex-col gap-6">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>
+                  Enter your name and email to proceed
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form onSubmit={handleSubmit} name="loginForm">
+                  <div className="flex flex-col gap-6">
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="name">Name</Label>
+                      </div>
+                      <Input
+                        name="name"
+                        id="name"
+                        type="name"
+                        required
+                        placeholder="Name"
+                        autoComplete="firstname"
+                        value={formData.name}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        name="email"
+                        id="email"
+                        type="email"
+                        placeholder="benjamin_franklin@example.com"
+                        autoComplete="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                  </div>
+                </form>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
