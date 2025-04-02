76f4a5edbca1e336413d01f6686c05b1cfe5240c
mjkushman <mjkushman@gmail.com>
2025-04-01 21:35:06 -0700

    switched dog card to shadcn

--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -1,34 +1,35 @@
 import { Dog } from "@/types/Dog";
+import { MapPin } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function DogCard({ dog }: { dog: Dog }) {
   return (
-    <div className="h-auto max-w-xs bg-white border border-gray-200 rounded-md shadow-sm overflow-hidden">
-      <div className="h-64 w-full overflow-hidden">
+    <>
+      <Card className="pt-0 m-2 rounded-t-md border-gray-50 gap-2 mx-0">
         <img
           src={dog.img}
           alt={dog.name}
-          className="w-full h-full object-cover object-center rounded-t-md"
+          className="w-full h-38 object-cover object-center rounded-t-md"
         />
-      </div>
-      <div className="p-3">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-          {dog.name}
-        </h5>
-        <div className="mb-3 font-normal text-gray-700 text-left py-1 px-2">
-          <p>
-            <strong>Age: </strong>
-            {dog.age}
-          </p>
-          <p>
-            <strong>Breed: </strong>
-            {dog.breed}
-          </p>
-          <p>
-            <strong>Zip Code:</strong>
-            {dog.zip_code}
-          </p>
-        </div>
-      </div>
-    </div>
+        <CardHeader className="gap-1">
+          <CardTitle className="text-lg">
+            {dog.name}, {dog.age}
+          </CardTitle>
+          <CardDescription>{dog.breed}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-row gap-4">
+            <MapPin />
+            <p>{dog.zip_code}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 }
