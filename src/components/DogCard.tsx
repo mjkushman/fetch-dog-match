d32f668ff455b4eaebec0d3266765cd44fd7cb14
mjkushman <mjkushman@gmail.com>
2025-04-02 21:55:56 -0700

    added skeleton for loading

--- src/components/DogCard.tsx
+++ src/components/DogCard.tsx
@@ -2,34 +2,44 @@ import { Dog } from "@/types/Dog";
 import { MapPin } from "lucide-react";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
-export default function DogCard({ dog }: { dog: Dog }) {
+export default function DogCard({
+  dog,
+  selected,
+}: {
+  dog: Dog;
+  selected: boolean;
+}) {
   return (
+    <Card
+      className={`p-0 rounded-t-md border-gray-50 gap-0 m-0 hover:shadow transition-all ${
+        selected
+          ? "border-2 border-blue-500 bg-blue-100 shadow-inner scale-95"
+          : ""
+      }`}
+    >
+      <img
+        src={dog.img}
+        alt={dog.name}
+        className="w-full h-38 object-cover object-center rounded-t-md"
+      />
+      <CardHeader className="gap-0 p-2">
+        <CardDescription className="">{dog.breed}</CardDescription>
+        <CardTitle className="text-lg pb-0 mb-0">
+          {dog.name}, {dog.age}
+        </CardTitle>
+      </CardHeader>
 
-      <Card className="pt-0 m-2 rounded-t-md border-gray-50 gap-2 mx-0 hover:shadow">
-        <img
-          src={dog.img}
-          alt={dog.name}
-          className="w-full h-38 object-cover object-center rounded-t-md"
-        />
-        <CardHeader className="gap-1">
-          <CardTitle className="text-lg">
-            {dog.name}, {dog.age}
-          </CardTitle>
-          <CardDescription>{dog.breed}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-row gap-4">
-            <MapPin />
-            <p>{dog.zip_code}</p>
-          </div>
-        </CardContent>
-      </Card>
-    
+      <CardDescription className="px-2 mb-1">
+        <div className="flex flex-row-reverse gap-1">
+          <p>{dog.zip_code}</p>
+          <MapPin className="w-4" />
+        </div>
+      </CardDescription>
+    </Card>
   );
 }
--- src/components/DogList.tsx
+++ src/components/DogList.tsx
@@ -1,30 +1,44 @@
 import { Dog } from "@/types/Dog";
 
 import DogCard from "./DogCard";
-
+import { Skeleton } from "./ui/skeleton";
 
 type DogListProps = {
   dogs: Dog[];
-  handleSelectId: (dog:Dog)=> void
+  handleSelectId: (dog: Dog) => void;
+  selectedDogs: Map<string, Dog>;
+  isLoading: boolean;
 };
 
-export default function DogList({ dogs,handleSelectId }: DogListProps) {
-  
-
-  
-
-  if (!dogs) return <div>Loading...</div>;
-  if (dogs.length == 0) return <div>No dogs found</div>;
+export default function DogList({
+  dogs,
+  handleSelectId,
+  selectedDogs,
+  isLoading,
+}: DogListProps) {
+  // if (dogs && dogs.length == 0) return <div>No dogs found</div>;
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-      {dogs &&
-        dogs.map((dog) => (
-          <div onClick={() => handleSelectId(dog)} key={dog.id}>
-            {" "}
-            <DogCard dog={dog} />
-          </div>
-        ))}
+    <div className="w-full min-h-[300px]">
+      {isLoading ? (
+        <div className="grid grid-flow-row md:grid-cols-4 xl:grid-cols-6 gap-4 px-6">
+          {Array.from({ length: 24 }).map((_, index) => (
+            <Skeleton key={index} className="h-52 w-58" />
+          ))}
+        </div>
+      ) : (
+        <div className="grid grid-flow-row md:grid-cols-4 xl:grid-cols-6 gap-4 px-6">
+          {dogs.map((dog) => {
+            const isSelected = selectedDogs.has(dog.id);
+            return (
+              <div onClick={() => handleSelectId(dog)} key={dog.id}>
+                {" "}
+                <DogCard dog={dog} selected={isSelected} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
